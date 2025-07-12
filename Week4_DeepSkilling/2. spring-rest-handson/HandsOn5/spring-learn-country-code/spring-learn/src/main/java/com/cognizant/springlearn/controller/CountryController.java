4fcbb8d325cdf01f996825f4862a6dfcaa73f315
Smayana-5 <smayanareddysasanoori@gmail.com>
2025-07-12 14:24:09 +0530

    Add files via upload

---
+++ Week4_DeepSkilling/2. spring-rest-handson/HandsOn5/spring-learn-country-code/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,28 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country in = (Country) context.getBean("in");
+        return in;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) throws CountryNotFoundException {
+        return countryService.getCountry(code);
+    }
+}
