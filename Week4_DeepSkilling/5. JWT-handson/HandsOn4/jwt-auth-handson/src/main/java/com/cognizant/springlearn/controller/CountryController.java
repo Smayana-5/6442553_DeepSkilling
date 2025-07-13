ef713760df316a5438e768ed71a810c975428e45
Smayana-5 <smayanareddysasanoori@gmail.com>
2025-07-13 17:55:34 +0530

    Add files via upload

---
+++ Week4_DeepSkilling/5. JWT-handson/HandsOn4/jwt-auth-handson/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,20 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/countries")
+    public List<Map<String, String>> getAllCountries() {
+        return List.of(
+            Map.of("code", "US", "name", "United States"),
+            Map.of("code", "DE", "name", "Germany"),
+            Map.of("code", "IN", "name", "India"),
+            Map.of("code", "JP", "name", "Japan")
+        );
+    }
+}
